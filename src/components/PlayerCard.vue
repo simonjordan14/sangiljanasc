8fe5f434cc99f449336d3e174d512a8765e629dd
Simon Jordan <simonjordan@Simons-MacBook-Pro.local>
2025-09-16 14:31:35 +0200

    adding pages and images

---
+++ src/components/PlayerCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import players from '../data/players';
+
+// define the event the child will emit
+const emit = defineEmits(["player-click"]);
+
+const handleClick = (player) => {
+  emit("player-click", player); // send player back to parent
+};
+</script>
+
+<template>
+  <div class="pt-5 flex justify-center">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 px-10">
+      <div
+        v-for="(player, index) in players"
+        :key="index"
+        class="mb-5 cursor-pointer text-center"
+        @click="handleClick(player)"
+      >
+        <img
+          :src="player.image"
+          :alt="player.name"
+          class="w-32 h-32 object-cover rounded-full pb-2 mx-auto"
+        />
+        <div>
+          <strong>{{ player.name }}</strong><br />
+          #{{ player.number }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
