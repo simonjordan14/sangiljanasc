36f12192da315529a83a51e5fa5d4cac879fa34d
Simon Jordan <simonjordan@Simons-MacBook-Pro.local>
2026-02-19 11:25:23 +0100

    added services section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,12 @@ import Header from '../components/Header.vue'
 import MainStory from '../components/MainStory.vue'
 import Sponsors from '../components/Sponsors.vue'
 import Footer from '../components/Footer.vue'
+import ServicesSection from '../components/ServicesSection.vue';
 </script>
 
 <template>
  <Header />
+  <ServicesSection />
   <MainStory />
   <Sponsors />
   <Footer />
